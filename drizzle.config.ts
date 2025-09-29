ea33739de5ed60734ad047343afc59bc4c5288e1
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-29 13:37:33 -0300

    subindo configuração do bun js + elyzia  + drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString:
+      process.env.DATABASE_URL ||
+      "postgres://user:password@localhost:5432/secret_santa",
+  },
+} satisfies Config;
